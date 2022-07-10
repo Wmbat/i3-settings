4b528c1560f5fa830d883ee51a36bab6c71f0e05
wmbat <wmbat-dev@protonmail.com>
2022-07-10 14:11:16 -0400

    Separate libi3-settings unit test into their own separate library called libi3-settings-tests

--- libi3-settings/tests/basics/settings.cpp
+++
@@ -1,9 +0,0 @@
-#include <catch2/catch_test_macros.hpp>
-
-#include <libi3-settings/settings.hpp>
-
-TEST_CASE("Loading config file", "[settings]")
-{
-	SECTION("Existing config file") {}
-	SECTION("Missing config file") {}
-}
--- libi3-settings/tests/basics/utils/file-io.cpp
+++
@@ -1,6 +0,0 @@
-#include <libi3-settings/utils/file-io.hpp>
-
-#include <catch2/catch_test_macros.hpp>
-
-#include <filesystem>
-
